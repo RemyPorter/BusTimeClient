aca0f185951b9e5630b575ca2de1a3946c6a7161
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-10-20 16:03:29 -0400

    Refactored stops and distance into their own files, still retained exports for API compatability.

--- bustime/__init__.py
+++ bustime/__init__.py
@@ -1,30 +1,11 @@
 import urllib.request as request
 import json
 import dateutil.parser
+from .distance import Distance
+from .stops import Stops
 
 base = "http://realtime.portauthority.org/bustime/api/v2/{method}?key={key}&format={format}"
 max_ids = 10
-class Stops:
-    """Manage stop data. Depends on a BusTime API object and a Distance API client.
-    """
-    def __init__(self, busapi, distanceapi):
-        self.api = busapi
-        self.dist = distanceapi
-
-    def stops_in_range(self, route, direction, location, distance=400, duration=None):
-        """Find all the stops for a given route and direction in range of a lat/long coordinate.
-        Location must be a dict in the form {lat:40.1234, lon:80.1234}.
-
-        Distance is measured in meters, duration is measured in seconds. If duration is supplied,
-        it will use the duration returned by the distance API, otherwise it will use the distance.
-        """
-        stops = self.api.getstops(route, direction)
-        distances = self.dist.distance_points(location, *stops)
-        if duration:
-            distance_item = lambda x: x[1]["value"] <= duration
-        else:
-            distance_item = lambda x: x[0]["value"] <= distance
-        return [s[2] for s in distances if distance_item(s)]
 
 class BustimeError(Exception): pass
 class BustimeParameterError(BustimeError): pass
@@ -126,36 +107,4 @@ class BusTime:
     def getrtpidatafeeds(self):
         return self.__callrest("getrtpidatafeeds")["rtpidatafeeds"]
 
-
-class Distance:
-    def __init__(self, google_key, window_size = 45):
-        self.key = google_key
-        self.window_size = window_size
-
-    def format_point(self, point):
-        pointfmt = "{lat},{lon}"
-        return pointfmt.format(**point)
-
-    def join_points(self, *points):
-        return "|".join([self.format_point(p) for p in points])
-
-    def distance_points(self, origin, *destinations):
-        """Given an origin and a list of destinations (in lat/lon form), returns a list of 3-tuples in the form "[(distance, duration, stop)]".
-        Distance and duration are dicts in the form {"value":n, "text":s}. Distance is in meters, duration in seconds.
-        The stop is a dictionary like: {'stpid': '2564', 'stpnm': '5th Ave  at Meyran Ave', 'lon': -79.959239533731, 'lat': 40.441172012068}"""
-        l = len(destinations) #we window the call so that we're not trying to pass 1,000 points on the URL and getting 400 errors. 45 appears to be a good default size.
-        if l < self.window_size:
-            return self.__distance_points(origin, *destinations)
-        else:
-            return list(self.__distance_points(origin, *destinations[:self.window_size])) + list(self.distance_points(origin, *destinations[self.window_size:]))
-
-    def __distance_points(self, origin, *destinations):
-        o = self.format_point(origin)
-        dests = self.join_points(*destinations)
-        base = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=walking&key={2}" #I should externalize this.
-        url = base.format(o, dests, self.key)
-        raw = request.urlopen(url).read()
-        data = json.loads(raw.decode("UTF8"))
-        data = data["rows"][0]["elements"]
-        return zip([e["distance"] for e in data], [e["duration"] for e in data], destinations)
-
+__all__ = [Stops, Distance, BusTime, BustimeError, BustimeParameterError]
