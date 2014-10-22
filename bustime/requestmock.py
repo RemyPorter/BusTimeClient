bf4c28c9c5edff852dc598398e1c1705d0bd36f8
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-10-22 12:07:01 -0400

    Oops, this should have been in bytes the whole time. Also moved the args to be a child of bustime-response.

--- bustime/requestmock.py
+++ bustime/requestmock.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse, parse_qs
-from io import StringIO
+from io import BytesIO
 import json
 
 class MockRequest:
@@ -9,25 +9,29 @@ class MockRequest:
         return path[start:]
 
     def urlopen(self, url):
-        output = StringIO()
+        output = BytesIO()
         parsed = urlparse(url)
         params = parse_qs(parsed.query)
         method = self.getmethod(parsed.path)
         assert("key" in params)
         s = getattr(self, method)(**params)
-        output.write(s)
+        output.write(s.encode("UTF8"))
         output.seek(0)
         return output
 
     def gettime(self, **kwargs):
         return json.dumps(
-            {"bustime-response": {"tm": "20141012 10:21:04"}, "_args":kwargs}
+            {"bustime-response": {"tm": "20141012 10:21:04","_args":kwargs}}
         )
 
     def getdirections(self, **kwargs):
         return json.dumps(
-            {"bustime-response": [{"dir":"INBOUND"}, {"dir":"OUTBOUND"}],
-            "_args":kwargs})
+            {"bustime-response": 
+                {"directions": [{"dir":"INBOUND"}, {"dir":"OUTBOUND"}],
+                    "_args":kwargs
+                }
+            }
+        )
 
     def getstops(self, **kwargs):
         return json.dumps(
