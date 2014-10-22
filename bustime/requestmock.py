780ebf45ab68faa658a131ff57be6b2783117958
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-10-22 12:55:53 -0400

    mocked getroutes

--- bustime/requestmock.py
+++ bustime/requestmock.py
@@ -26,7 +26,7 @@ class MockRequest:
 
     def getdirections(self, **kwargs):
         return json.dumps(
-            {"bustime-response": 
+            {"bustime-response":
                 {"directions": [{"dir":"INBOUND"}, {"dir":"OUTBOUND"}]}
             }
         )
@@ -34,7 +34,7 @@ class MockRequest:
     def getstops(self, **kwargs):
         return json.dumps(
             {
-                "bustime-response": 
+                "bustime-response":
                 {"stops": [
                 {'stpid': '2564', 'stpnm': '5th Ave  at Meyran Ave',
                 'lon': -79.959239533731, 'lat': 40.441172012068}]}
@@ -43,25 +43,32 @@ class MockRequest:
     def getpredictions(self, **kwargs):
         return json.dumps({
             "bustime-response":{
-                "prd": [{'rt': '71C', 'typ': 'A', 
-                'prdctdn': '5', 'prdtm': '20141022 12:37', 
-                'rtdir': 'INBOUND', 'zone': '', 
-                'des': 'Downtown', 'dly': False, 
-                'dstp': 4198, 'stpnm': '5th Ave at Chesterfield Rd', 
-                'stpid': '38', 'tatripid': '159261', 
-                'tmstmp': '20141022 12:31', 'tablockid': '071C-150', 
+                "prd": [{'rt': '71C', 'typ': 'A',
+                'prdctdn': '5', 'prdtm': '20141022 12:37',
+                'rtdir': 'INBOUND', 'zone': '',
+                'des': 'Downtown', 'dly': False,
+                'dstp': 4198, 'stpnm': '5th Ave at Chesterfield Rd',
+                'stpid': '38', 'tatripid': '159261',
+                'tmstmp': '20141022 12:31', 'tablockid': '071C-150',
                 'vid': '5678'}]}
             })
 
     def getvehicles(self, **kwargs):
         return json.dumps({
             "bustime-response":{
-                "vehicle":[{'tablockid': '071C-148', 'pid': 2363, 
-                'tatripid': '159264', 'zone': '', 'des': 'Downtown', 
-                'dly': False, 'spd': 0, 'pdist': 17607, 'hdg': '299', 
-                'tmstmp': '20141022 12:52', 'lat': '40.46042251586914', 
+                "vehicle":[{'tablockid': '071C-148', 'pid': 2363,
+                'tatripid': '159264', 'zone': '', 'des': 'Downtown',
+                'dly': False, 'spd': 0, 'pdist': 17607, 'hdg': '299',
+                'tmstmp': '20141022 12:52', 'lat': '40.46042251586914',
                 'rt': '71C', 'vid': '5669', 'lon': '-79.92157814719461'}]
             }
+            })
+
+    def getroutes(self, **kwargs):
+        return json.dumps({
+                "bustime-response": {
+                    "routes": [{'rt': '12', 'rtnm': 'MCKNIGHT', 'rtclr': '#cc00cc'}]
+                }
             })
 
 
